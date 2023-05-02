3fd711d29344f01e626785b3b5ec7ebd73fb8087
mr-ema <ema.a.v.c2017@gmail.com>
2023-05-02 11:43:41 -0400

    add library

---
+++ lib/README.md
@@ -0,0 +1,12 @@
+# MyLib
+To avoid rewriting the same code repeatedly, I have defined the necessary structures here. This way, whenever I
+need a queue, I can simply include the code and use the pre-defined queue without having to start from scratch.
+
+</br>
+
+## Important
+I'm using [acutest.h](https://github.com/mity/acutest/blob/master/include/acutest.h) and
+[list.h](https://github.com/torvalds/linux/blob/master/include/linux/list.h) as examples for my implementation.
+However, I want to emphasize that I am not an expert in C and this is a basic implementation.
+If you are creating libraries, I recommend looking at these examples and doing your own research.
+
---
+++ lib/queue.h
@@ -0,0 +1,82 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#define MAX_ARRAY_SIZE 20 
+
+#include <stdio.h>
+
+typedef struct {
+        int rear;
+        int front;
+        void* queue[MAX_ARRAY_SIZE];
+} queue_t;
+
+static inline void queue_init(queue_t *q) {
+        q->rear = -1;
+        q->front = -1;
+}
+
+static inline int queue_is_empty(queue_t *q) {
+        return (q->rear <= -1 && q->front <= -1);
+}
+
+static inline int queue_is_full(queue_t *q) {
+        return (q->rear >= MAX_ARRAY_SIZE - 1);
+}
+
+static inline int queue_len(queue_t *q) {
+        if (queue_is_empty(q)) {
+                return 0;
+        }
+
+        return (q->rear - q->front) + 1;
+}
+
+static inline void* queue_peek(queue_t *q) {
+        if (queue_is_empty(q)) {
+                return NULL;
+        }
+
+        return q->queue[q->front];
+}
+
+static inline void queue_enqueue(queue_t *q, void* value) {
+        if (queue_is_full(q)) {
+                return;
+        } else if (queue_is_empty(q)) {
+                q->front++;
+        }
+
+        q->queue[++q->rear] = value;
+}
+
+static inline void* queue_dequeue(queue_t *q) {
+        if (queue_is_empty(q)) {
+                return NULL;
+        }
+
+        void* temp = q->queue[q->front];
+
+        if (q->front == q->rear) {
+                q->front = -1;
+                q->rear = -1;
+        } else {
+                q->front++;
+        }
+
+        return temp;
+}
+
+static inline void queue_display(queue_t *q) {
+        if (queue_is_empty(q)) {
+                printf("Queue Is Empty\n");
+                return;
+        }
+
+        for (int i = q->front; i <= q->rear; i++)
+                printf("|%p", q->queue[i]);
+        printf("|\n");
+}
+
+#endif /* QUEUE_H */
+
