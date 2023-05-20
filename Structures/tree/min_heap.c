373f1afbc32c35d6f1361d72f0f55c9311c60972
mr-ema <ema.a.v.c2017@gmail.com>
2023-05-20 16:29:26 -0400

    feat(tree): add minimum binary heap

--- Structures/tree/min_heap.c
+++ Structures/tree/min_heap.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
+
+/* MINIMUM BINARY HEAP */
 
 typedef struct Node {
         struct Node *Left;
@@ -69,7 +72,7 @@ int right_child(int index)
  * @param capacity The maximum number of elements the heap can hold.
  * @return A pointer to the newly created heap.
  */
-MinHeap* min_heap_init(int capacity)
+MinHeap* minheap_init(int capacity)
 {
         MinHeap *heap = malloc(sizeof(MinHeap));
         if (!heap) {
@@ -95,7 +98,7 @@ MinHeap* min_heap_init(int capacity)
  *
  * @param heap A pointer to the pointer of the min heap to be deallocated.
  */
-void min_heap_deinit(MinHeap **heap)
+void minheap_deinit(MinHeap **heap)
 {
         free((*heap)->Arr);     // Free the memory for the array
         free(*heap);            // Free the memory for the MinHeap struct
@@ -109,23 +112,57 @@ void min_heap_deinit(MinHeap **heap)
  * @param heap A pointer to the min heap structure.
  * @return The minimum key in the min heap.
  */
-int min_heap_get_min(MinHeap *heap)
+int minheap_peek(MinHeap *heap)
 {
         return (heap->Arr[0]);
 }
 
 /**
- * Performs the heapify operation on a min heap represented as an array.
+ * Performs the "heapify up" operation to maintain the min heap property
+ * after inserting an element at a specific index.
  *
  * @param arr The array representing the min heap.
- * @param idx The index of the current node to heapify.
+ * @param idx The index of the element to be heapified.
  */
-void min_heap_heapify(int* arr, size_t idx)
+void minheap_heapify_up(MinHeap *heap, size_t idx)
 {
+        int *arr = heap->Arr;
+
         while (idx > 0 && arr[parent(idx)] > arr[idx]) {
-                xor_swap(&(arr[idx]), &(arr[parent(idx)]));
+                xor_swap(&arr[idx], &arr[parent(idx)]);
                 idx = parent(idx);
         }
+}
+
+/**
+ * Performs the "heapify down" operation to maintain the min heap property
+ * after deleting an element at a specific index.
+ *
+ * @param heap A pointer to the MinHeap structure.
+ * @param idx  The index of the element to be heapified.
+ */
+void minheap_heapify_down(MinHeap *heap, size_t idx)
+{
+        int *arr    = heap->Arr;
+        size_t size = heap->Size;
+
+        while (1) {
+                size_t smallest = idx;
+                size_t left = left_child(idx);
+                size_t right = right_child(idx);
+
+                if (left < size && arr[left] < arr[smallest])
+                        smallest = left;
+
+                if (right < size && arr[right] < arr[smallest])
+                        smallest = right;
+
+                if (smallest == idx)
+                        break;
+
+                xor_swap(&arr[idx], &arr[smallest]);
+                idx = smallest;
+    }
 }
 
 /**
@@ -134,7 +171,7 @@ void min_heap_heapify(int* arr, size_t idx)
  * @param heap A pointer to the minimum heap structure.
  * @param key The key to be inserted.
  */
-void min_heap_insert(MinHeap *heap, int key)
+void minheap_insert(MinHeap *heap, int key)
 {
         if (heap->Capacity == heap->Size) {
                 perror("overflow: could not insert key");
@@ -146,12 +183,71 @@ void min_heap_insert(MinHeap *heap, int key)
         heap->Arr[idx] = key;
 
         // Maintain the min heap property by swapping the element with its parent as long as necessary
-        while (idx > 0 && heap->Arr[parent(idx)] > heap->Arr[idx]) {
-                xor_swap(&(heap->Arr[idx]), &(heap->Arr[parent(idx)]));
-                idx = parent(idx);
-        }
+        minheap_heapify_up(heap, idx);
 }
 
-void min_heap_delete(MinHeap *heap, int key) {}
+/**
+ * Deletes an element at the specified index from the MinHeap structure.
+ *
+ * @param heap Pointer to the MinHeap structure.
+ * @param idx Index of the element to be deleted.
+ */
+void minheap_delete_idx(MinHeap *heap, int idx)
+{
+        if (heap->Size <= 0) {
+                perror("underflow: heap is empty");
+                return;
+        } else if (idx >= heap->Size) {
+                perror("invalid index: index is out of bounds");
+                return;
+        }
 
-void min_heap_peek(MinHeap *heap) {}
+        heap->Arr[idx] = heap->Arr[heap->Size - 1];
+        heap->Size--;
+
+        if (idx == 0 || heap->Arr[idx] > heap->Arr[parent(idx)])
+                minheap_heapify_down(heap, idx);
+        else
+                minheap_heapify_up(heap, idx);
+}
+
+// Test
+void run_tests()
+{
+    // Test minheap_init and minheap_peek
+    MinHeap* heap = minheap_init(5);
+    assert(heap != NULL);
+    assert(heap->Size == 0);
+    assert(heap->Capacity == 5);
+
+    minheap_insert(heap, 10);
+    minheap_insert(heap, 5);
+    minheap_insert(heap, 7);
+    minheap_insert(heap, 2);
+    minheap_insert(heap, 9);
+    printf("Insertion tests passed.\n");
+
+    printf("Peek test passed.\n");
+
+    // Test minheap_delete_idx
+    minheap_delete_idx(heap, 2);
+    assert(minheap_peek(heap) == 2);
+    printf("Delete test 1 passed.\n");
+
+    minheap_delete_idx(heap, 0);
+    assert(minheap_peek(heap) == 5);
+    printf("Delete test 2 passed.\n");
+
+    // Test minheap_deinit
+    minheap_deinit(&heap);
+    assert(heap == NULL);
+    printf("Deallocation test passed.\n");
+}
+
+int main(int argc, char *argv[])
+{
+        run_tests();
+        printf("All tests passed successfully.\n");
+
+        return 0;
+}
